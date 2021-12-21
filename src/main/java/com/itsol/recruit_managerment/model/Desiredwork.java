07c2f9ff71c4b74713981e836ebaaab0c1d4bb6f
Anhphuc9969 <huuphuc9969@gmail.com>
2021-12-21 19:37:01 +0700

    no message

--- src/main/java/com/itsol/recruit_managerment/model/Desiredwork.java
+++ src/main/java/com/itsol/recruit_managerment/model/Desiredwork.java
@@ -15,12 +15,14 @@ import java.io.Serializable;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 @Table(name = "desiredwork")
-public class Desiredwork implements Serializable {
+public class DesiredWork implements Serializable {
     @Id
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DESIRED_WORK_SEQ")
     @SequenceGenerator(name = "DESIRED_WORK_SEQ", sequenceName = "DESIRED_WORK_SEQ", allocationSize = 1, initialValue = 1)
     Long id;
+    @OneToOne(mappedBy = "desiredwork", fetch = FetchType.EAGER)
+    Profiles profiles;
 
     @Column(name = "desired_work_name ", nullable = false)
     String desiredworkname;
