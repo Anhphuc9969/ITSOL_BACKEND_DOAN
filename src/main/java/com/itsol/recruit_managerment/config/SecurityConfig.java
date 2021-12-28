c5a73e141d0946127aa908db10f9b14fd31488ad
Huybg234 <nqhuybg234@gmail.com>
2021-12-28 19:18:15 +0700

    huy

--- src/main/java/com/itsol/recruit_managerment/config/SecurityConfig.java
+++ src/main/java/com/itsol/recruit_managerment/config/SecurityConfig.java
@@ -43,16 +43,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/signup").permitAll();
         http.authorizeRequests().antMatchers("/active/**").permitAll();
-//        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/user/**,").hasAnyAuthority("ROLE_JE", "ROLE_USER", "ROLE_ADMIN");
-//        http.authorizeRequests().antMatchers("/api/admin/**").hasAnyAuthority("ROLE_JE","ROLE_ADMIN");
-//        http.authorizeRequests().antMatchers("/**").hasAnyAuthority("ROLE_ADMIN");
-//        http.authorizeRequests().anyRequest().authenticated();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/user/**,").hasAnyAuthority("ROLE_JE", "ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/api/admin/**").hasAnyAuthority("ROLE_JE","ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+//        http.authorizeRequests().anyRequest().permitAll();
         http.addFilter(new AuthenFilter(authenticationManager(), appUserService));
         http.addFilterBefore(new AuthorFilter(), UsernamePasswordAuthenticationFilter.class);
-//        http.formLogin()
-//                .loginProcessingUrl("/login")
-//                .isCustomLoginPage();
+        http.formLogin()
+                .loginProcessingUrl("/login")
+                .isCustomLoginPage();
         http.logout()
                 .logoutUrl("/logout")
                 .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.MULTI_STATUS));
